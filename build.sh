b37a9fb4b9020f0f46c0c46fb8da51ef810a300d
Mohamed Mahrous <m.mahrous.94@gmail.com>
2020-03-25 01:58:05 +0200

    update build.sh to take bucket name

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 echo "Pulling model from s3"
+bucket=$1
 tag=$3
 model=$2/$tag
-mkdir -p $model
-aws s3 cp s3://project-sea-cv-models/$model models/$model --recursive
+mkdir -p models/$model
+aws s3 cp s3://$bucket/$model models/$model --recursive
 echo "Build docker image"
-nvidia-docker build -t --build-args MODEL=models/$model vc:$tag
+nvidia-docker build -t vc:$tag --build-arg MODEL=models/$model .
 echo "Happy counting 🚗"
