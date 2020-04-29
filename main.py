4ca983ec75491a13b28435d57cf3463a755f764f
BishoyM <arcane.arts7@gmail.com>
2020-04-29 15:26:09 +0200

    fix: Use h264 encoding in cv2 videoWriter

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def run():
     if record:
         # initialize video object to record counting
         output_video = cv2.VideoWriter(out_video_path,
-                                       cv2.VideoWriter_fourcc(*'MP4V'),
+                                       cv2.VideoWriter_fourcc(*'X264'),
                                        30,
                                        (f_width, f_height))
 
