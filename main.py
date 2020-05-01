09bff0b83571c50a90e9a8d02e4cb81eacab7a97
Mohamed Mahrous <m.mahrous.94@gmail.com>
2020-05-01 04:26:37 +0200

    convert avi to mp4

--- main.py
+++ main.py
@@ -251,7 +251,8 @@ def run():
                 add_db_index.status_code, create_video_mutation, add_db_index.json()))
 
     def convert_avi_to_mp4(avi_file_path, output_name):
-        os.popen("ffmpeg -i '{input}' -ac 2 -b:v 2000k -c:a aac -c:v libx264 -b:a 160k -vprofile high -bf 0 -strict experimental -f mp4 '{output}'".format(input=avi_file_path, output=output_name))
+        os.system("ffmpeg -i '{input}' -ac 2 -b:v 2000k -c:a aac -c:v libx264 -b:a 160k -vprofile high -bf 0 -strict experimental -f mp4 '{output}'".format(
+            input=avi_file_path, output=output_name))
         return True
 
     # Trigger upload process
